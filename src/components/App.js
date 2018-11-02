340d5ae819e94ec86fe65ca19dfbf1a88066f9fc
Michael Chang <chan302@usc.edu>
2018-11-02 16:03:06 -0700

    'saving'

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,12 @@
 import React, { Component } from 'react';
-<<<<<<< HEAD
+
 import {BrowserRouter,Router, Switch} from 'react-router-dom';
 import Route from 'react-router-dom/Route';
 import Footer from '../components/Footer';
 import Body from '../components/Body';
 import Business from '../components/Business';
-=======
-import Footer from '../components/Footer';
-import Body from '../components/Body';
 
->>>>>>> a54987bc08c3974e5c8e6897531076faa212c040
+
 import logo from '../images/logo.svg';
 import '../styles/App.css';
 
@@ -22,6 +19,8 @@ class App extends Component {
 
               <Route exact path="/" component={Body} />
               <Route exact path='/business' component={Business}/>
+              {/*<Route exact path="/faq" component={} />
+              <Route exact path="/termsofservices" component={} />*/}
 
           </div>
         </BrowserRouter>
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,37 @@
 import React, { Component } from 'react';
-//import{ Popover} from 'react-bootstrap';
+
+import {Button} from 'react-bootstrap';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import ResponsiveMenu from 'react-responsive-navbar';
+import '../styles/footer.css';
 
 class Footer extends Component{
   render(){
     return (
-      <div className="footer">
-          <h3> Footer </h3>
-      </div>
+      <Router>
+        <div>
+
+
+          <div className='footer'>
+            <ResponsiveMenu
+
+              changeMenuOn="500px"
+              largeMenuClassName="footer-large-menu-classname"
+              smallMenuClassName="footer-small-menu-classname"
+              menu={
+
+                <ul>
+                  <li>  <Link to="/faq"> FAQ </Link> </li>
+                  <li>  <Link to="/contact"> Contact Us </Link> </li>
+                  <li>  <Link to="/TOS"> Terms of Services </Link> </li>
+                </ul>
+
+              }
+            />
+          </div>
+
+        </div>
+      </Router>
     );
   }
 }
